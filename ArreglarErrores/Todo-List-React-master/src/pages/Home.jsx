689e39fdd5db3637224f91ef64c5d370b6eb4862
Jflores-2019424 <107268421+Jflores-2019424@users.noreply.github.com>
2023-06-16 22:54:40 -0600

    Add files via upload

---
+++ ArreglarErrores/Todo-List-React-master/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Task} from '../components/task'
+
+export const Home = () => {
+  return (
+    <>
+      <div className="container">
+        <h1 className='text-lx'>Home</h1>
+        <button>Ir a las tareas</button>
+        <br /><br /><br /><br /><br />
+        <hr />
+        <Task></Task>
+      </div>
+    </>
+  )
+}
